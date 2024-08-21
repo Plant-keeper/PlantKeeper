c527a9f76a90e5c6f3dfc44c78fbc91c2416c80a
Sinaf19 <quentinsurdez@gmail.com>
2024-08-21 14:35:51 +0200

    frontend + backend init

---
+++ backend/dist/items/dto/create-item.dto.d.ts
@@ -0,0 +1,5 @@
+export declare class CreateItemDto {
+    id: number;
+    name: string;
+    price: number;
+}
---
+++ backend/src/items/dto/create-item.dto.ts
@@ -0,0 +1,5 @@
+export class CreateItemDto {
+    id: number;
+    name: string;
+    price: number;
+}
