5b08767280126612ff9c7c6e4ba3d6dd70bdb60e
Rafou98 <95037521+Rafou28@users.noreply.github.com>
2024-08-23 15:03:17 +0200

    Add serialization to JSON

--- sensors/arduino_code/src/main.cpp
+++ sensors/arduino_code/src/main.cpp
@@ -2,31 +2,47 @@
 #include "DHT.h"
 #include "ArduinoJson.h"
 
+// Instantiate DHT and JSon object
 DHT dht(2, DHT11);
+JsonDocument doc;
+// Global variables used in loop
+int val1;
+int val2;
+int percentage1;
+int percentage2;
+float h;
+float t;
 
 const int dry = 1023;
 const int wet = 700;
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup()
 {
-  // put your setup code here, to run once:
+
   dht.begin();
   Serial.begin(115200);
 }
 
 void loop()
 {
+  // Read sensor values
+  val1 = analogRead(0);
+  val2 = analogRead(1);
 
-  int val1 = analogRead(0);
-  int val2 = analogRead(1);
+  h = dht.readHumidity();
+  t = dht.readTemperature();
 
-  int percentage1 = map(val1, dry, wet, 0, 100);
-  int percentage2 = map(val2, dry, wet, 0, 100);
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  // Map sensor values to percentage
+  percentage1 = map(val1, dry, wet, 0, 100);
+  percentage2 = map(val2, dry, wet, 0, 100);
+
+  // Create JSON object
+  doc["temperature"] = t;
+  doc["humidity"] = h;
+  doc["val1"] = val1;
+  doc["val2"] = val2;
+  doc["moisture1"] = percentage1;
+  doc["moisture2"] = percentage2;
 
   Serial.print("Humidity: ");
   Serial.println(h);
@@ -42,11 +58,10 @@ void loop()
   Serial.println(percentage2);
   Serial.print("Pot : ");
   Serial.println(analogRead(A2));
-  delay(50);
-}
 
-// put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
+  // Serialize JSON object to Serial
+  serializeJson(doc, Serial);
+  Serial.println();
+
+  delay(500);
 }
