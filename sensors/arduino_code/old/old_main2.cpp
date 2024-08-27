1066f06fb628393e6caf7b81ad145fa89d14c455
Rafou98 <95037521+Rafou28@users.noreply.github.com>
2024-08-27 22:54:41 +0200

    feat: the connection in mode AP then the connection to a networks are functional.

---
+++ sensors/arduino_code/old/old_main2.cpp
@@ -0,0 +1,210 @@
+ /**
+ * @file main.cpp
+ * @brief: Program that reads temperature, humidity and soil moisture values from sensors and sends them to a server
+ * 
+ * @author: Rafael Dousse
+ * @date 23.08.2024
+ * @version 1.0
+ */
+
+#include <Arduino.h>
+#include "DHT.h"
+#include "ArduinoJson.h"
+
+#include <WiFiNINA.h>
+#include <arduino_secrets.h>
+#include <webpage.h>
+
+// Function prototypes
+void enable_WiFi();
+void connect_WiFi();
+void printWifiStatus();
+void printWEB(float t,float h ,int percentage1,int percentage2);
+
+// Credentials
+const char *wifi[] = {"SamsungR", "123456789"};
+
+// Network parameters
+int keyIndex = 0;            // Network key Index number (needed only for WEP)
+int status = WL_IDLE_STATUS; // Connection status
+WiFiServer server(80);       // Server socket
+WiFiClient client = server.available();
+String header;
+
+// Instantiate DHT and JSon object
+DHT dht(2, DHT11);
+JsonDocument doc;
+// Global variables used in loop
+int val1;
+int val2;
+int percentage1;
+int percentage2;
+float h;
+float t;
+
+const int dry = 1023;
+const int wet = 700;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  enable_WiFi();
+  connect_WiFi();
+  server.begin();
+  printWifiStatus();
+  dht.begin();
+
+}
+
+void loop()
+{
+ 
+  // Read sensor values
+  val1 = analogRead(0);
+  val2 = analogRead(1);
+
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+
+  // Map sensor values to percentage
+  percentage1 = map(val1, dry, wet, 0, 100);
+  percentage2 = map(val2, dry, wet, 0, 100);
+
+  // Create JSON object
+  doc["temperature"] = t;
+  doc["humidity"] = h;
+  doc["val1"] = val1;
+  doc["val2"] = val2;
+  doc["moisture1"] = percentage1;
+  doc["moisture2"] = percentage2;
+
+  Serial.print("Humidity: ");
+  Serial.println(h);
+  Serial.print("Temperature: ");
+  Serial.println(t);
+  Serial.print("Moisture Level: ");
+  Serial.println(val1);
+  Serial.println("Percentage1: ");
+  Serial.println(percentage1);
+  Serial.print("Moisture Level2: ");
+  Serial.println(val2);
+  Serial.println("Percentage2: ");
+  Serial.println(percentage2);
+  Serial.print("Pot : ");
+  Serial.println(analogRead(A2));
+
+  // Serialize JSON object to Serial
+  serializeJson(doc, Serial);
+  Serial.println();
+
+
+  client = server.available();
+
+    if (client) {
+        printWEB(t, h, percentage1, percentage2);
+    }
+
+  delay(500);
+}
+
+
+/**
+ * @brief: Function that prints Wi-Fi connection information to the serial monitor.
+ */
+void printWifiStatus() {
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());  
+
+    IPAddress ip = WiFi.localIP(); 
+    Serial.print("IP Address: ");
+    Serial.println(ip); 
+
+    long rssi = WiFi.RSSI(); 
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");  
+
+    Serial.print("To see this page in action, open a browser to http://");
+    Serial.println(ip); 
+}
+
+/**
+ * Function that checks if the Wi-Fi module is available.
+ * If the module is not found, the program stops.
+ */
+void enable_WiFi() {
+    if (WiFi.status() == WL_NO_MODULE) {
+        Serial.println("Communication with WiFi module failed!");
+        // TODO: Change to a more elegant solution
+        while (true);  // Infinite loop if Wi-Fi module is absent 
+    }
+
+    String fv = WiFi.firmwareVersion();
+    if (fv < "1.0.0") {
+        Serial.println("Please upgrade the firmware");  // Warn if the firmware is outdated
+    }
+}
+// TODO: Change the wifi[0] to another system
+/**
+ * Function that attempts to connect to the Wi-Fi network.
+ */
+void connect_WiFi() {
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to SSID: ");
+        Serial.println(wifi[0]); 
+        status = WiFi.begin(wifi[0], wifi[1]);
+        delay(5000);  
+
+        if (status == WL_CONNECTED) {
+            break;  // Exit the loop if connected
+        }
+
+    }
+}
+
+/**
+ * Function that handles the HTTP response to display a web page.
+ * It receives client requests, sends the HTML page, and manages the disconnection.
+ */
+void printWEB(float t, float h, int percentage1, int percentage2) {
+    if (client) {  
+        Serial.println("new client");
+        String currentLine = "";  
+        while (client.connected()) {  
+            if (client.available()) {  
+                char c = client.read();  // Read a character from the request
+                Serial.write(c); 
+                header += c;  // Append the character to the header
+
+                if (c == '\n') { 
+                    if (currentLine.length() == 0) {
+                        // If the line is blank, it's the end of the HTTP request, so send the response
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println("Connection: close");
+                        client.println();
+
+                        // Send the HTML page to the client
+                        client.println("<!DOCTYPE html><html>");
+                        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                        client.println("<link rel=\"icon\" href=\"data:,\">");
+                        String webpage = generateWebpage(t, h, percentage1, percentage2);
+                        client.println(webpage);  // Send the generated webpage
+                      
+                        client.println();
+
+                        break;  
+                    } else {  
+                        currentLine = "";  // Reset the current line
+                    }
+                } else if (c != '\r') {   
+                    currentLine += c;  // Append the character to the current line if it's not a carriage return
+                }
+            }
+        }
+        header = "";  // Reset the header after processing
+        client.stop();  // Close the connection with the client
+        Serial.println("client disconnected");
+    }
+} 
--- sensors/arduino_code/src/main.cpp
+++ sensors/arduino_code/src/main.cpp
@@ -1,214 +1,368 @@
 /**
  * @file main.cpp
  * @brief: Program that reads temperature, humidity and soil moisture values from sensors and sends them to a server
- * 
+ *
  * @author: Rafael Dousse
  * @date 23.08.2024
  * @version 1.0
  */
-
 #include <Arduino.h>
-#include "DHT.h"
-#include "ArduinoJson.h"
-
 #include <WiFiNINA.h>
-#include <arduino_secrets.h>
-#include <webpage.h>
+#include "arduino_secrets.h"
+#include <vector>
+#include "webpages.h"
+// To check how it works, if time allows
+/* #include <WiFiUdp.h>
+#include <ArduinoMDNS.h> */
 
 // Function prototypes
-void enable_WiFi();
-void connect_WiFi();
-void printWifiStatus();
-void printWEB(float t,float h ,int percentage1,int percentage2);
+void startAccessPoint();
+void handleConfigRequest();
+void connectToWiFi();
+void printWEB();
 
-// Credentials
-const char *wifi[] = {SECRET_SSID, SECRET_PASS};
+// Global variables
+char ssid[32];
+char pass[64];
+bool needsWiFiConfig = true;
 
-// Network parameters
-int keyIndex = 0;            // Network key Index number (needed only for WEP)
-int status = WL_IDLE_STATUS; // Connection status
-WiFiServer server(80);       // Server socket
-WiFiClient client = server.available();
-String header;
+const char *ssidArduino = "Arduino_Config";
+const char *passArduino = "password";
 
-// Instantiate DHT and JSon object
-DHT dht(2, DHT11);
-JsonDocument doc;
-// Global variables used in loop
-int val1;
-int val2;
-int percentage1;
-int percentage2;
-float h;
-float t;
+WiFiServer server(80); // Server socket
+/* WiFiClient client; */
+int status = WL_IDLE_STATUS;
+WiFiClient client;
+std::vector<const char *> networks;
 
-const int dry = 1023;
-const int wet = 700;
+/* WiFiUDP udp;
+MDNS mdns(udp);
+ */
+/* void nameFound(const char* name, IPAddress ip)
+{
+  if (ip != INADDR_NONE) {
+    Serial.print("The IP address for '");
+    Serial.print(name);
+    Serial.print("' is ");
+    Serial.println(ip);
+  } else {
+    Serial.print("Resolving '");
+    Serial.print(name);
+    Serial.println("' timed out.");
+  }
+} */
+
+void listNetworks()
+{
+    // scan for nearby networks:
+    Serial.println("** Scan Networks **");
+
+    int numSsid = WiFi.scanNetworks();
+    if (numSsid == -1)
+    {
+        Serial.println("Couldn't get a WiFi connection");
+        while (true)
+            ;
+    }
+
+    // print the list of networks seen:
+    Serial.print("number of available networks:");
+    Serial.println(numSsid);
+
+    // print the network number and name for each network found:
+    for (int thisNet = 0; thisNet < numSsid; thisNet++)
+    {
+        networks.push_back(WiFi.SSID(thisNet));
+    }
+
+}
 
 void setup()
 {
-  Serial.begin(115200);
 
-  enable_WiFi();
-  connect_WiFi();
-  server.begin();
-  printWifiStatus();
-  dht.begin();
+    Serial.begin(9600);
 
+    Serial.println("Starting...");
+
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        listNetworks();
+        startAccessPoint(); // Start in AP mode to configure WiFi
+    }
+    else
+    {
+        connectToWiFi(); // Connect to WiFi if already configured
+    }
 }
 
 void loop()
 {
- 
+    /* const char *hostName = "MyArduino";
+    int length = strlen(hostName);
+      if (!mdns.isResolvingName()) {
+        if (length > 0) {
+          Serial.print("Resolving '");
+          Serial.print(hostName);
+          Serial.println("' via Multicast DNS (Bonjour)...");
 
+          // Now we tell the mDNS library to resolve the host name. We give it a
+          // timeout of 5 seconds (e.g. 5000 milliseconds) to find an answer. The
+          // library will automatically resend the query every second until it
+          // either receives an answer or your timeout is reached - In either case,
+          // the callback function you specified in setup() will be called.
 
-
-  // Read sensor values
-  val1 = analogRead(0);
-  val2 = analogRead(1);
-
-  h = dht.readHumidity();
-  t = dht.readTemperature();
-
-  // Map sensor values to percentage
-  percentage1 = map(val1, dry, wet, 0, 100);
-  percentage2 = map(val2, dry, wet, 0, 100);
-
-  // Create JSON object
-  doc["temperature"] = t;
-  doc["humidity"] = h;
-  doc["val1"] = val1;
-  doc["val2"] = val2;
-  doc["moisture1"] = percentage1;
-  doc["moisture2"] = percentage2;
-
-  Serial.print("Humidity: ");
-  Serial.println(h);
-  Serial.print("Temperature: ");
-  Serial.println(t);
-  Serial.print("Moisture Level: ");
-  Serial.println(val1);
-  Serial.println("Percentage1: ");
-  Serial.println(percentage1);
-  Serial.print("Moisture Level2: ");
-  Serial.println(val2);
-  Serial.println("Percentage2: ");
-  Serial.println(percentage2);
-  Serial.print("Pot : ");
-  Serial.println(analogRead(A2));
-
-  // Serialize JSON object to Serial
-  serializeJson(doc, Serial);
-  Serial.println();
-
-
-  client = server.available();
-
-    if (client) {
-        printWEB(t, h, percentage1, percentage2);
+          mdns.resolveName(hostName, 5000);
+        }
+      }
+       mdns.run(); */
+    if (needsWiFiConfig)
+    {
+        client = server.available();
+        if (client)
+        {
+            handleConfigRequest();
+        }
     }
-
-  delay(500);
-}
-
-
-/**
- * @brief: Function that prints Wi-Fi connection information to the serial monitor.
- */
-void printWifiStatus() {
-    Serial.print("SSID: ");
-    Serial.println(WiFi.SSID());  
-
-    IPAddress ip = WiFi.localIP(); 
-    Serial.print("IP Address: ");
-    Serial.println(ip); 
-
-    long rssi = WiFi.RSSI(); 
-    Serial.print("signal strength (RSSI):");
-    Serial.print(rssi);
-    Serial.println(" dBm");  
-
-    Serial.print("To see this page in action, open a browser to http://");
-    Serial.println(ip); 
+    else
+    {
+        client = server.available();
+        if (client)
+        {
+            Serial.println("Client available... so printweb");
+            printWEB();
+        }
+    }
 }
 
 /**
- * Function that checks if the Wi-Fi module is available.
- * If the module is not found, the program stops.
+ * Function to start the Arduino in Access Point mode
  */
-void enable_WiFi() {
-    if (WiFi.status() == WL_NO_MODULE) {
+void startAccessPoint()
+{
+    Serial.println("Starting Access Point...");
+    WiFi.end();
+
+    if (WiFi.status() == WL_NO_MODULE)
+    {
         Serial.println("Communication with WiFi module failed!");
-        // TODO: Change to a more elegant solution
-        while (true);  // Infinite loop if Wi-Fi module is absent 
+
+        while (true)
+            ;
     }
 
     String fv = WiFi.firmwareVersion();
-    if (fv < "1.0.0") {
-        Serial.println("Please upgrade the firmware");  // Warn if the firmware is outdated
+    if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+    {
+        Serial.println("Please upgrade the firmware");
     }
+
+    int status = WL_IDLE_STATUS;
+
+    status = WiFi.beginAP(ssidArduino, passArduino);
+
+    if (status != WL_AP_LISTENING)
+    {
+        Serial.println("Creating access point failed");
+
+        while (true)
+            ;
+    }
+    /*
+        mdns.begin(WiFi.localIP(), "arduino");
+
+      // We specify the function that the mDNS library will call when it
+      // resolves a host name. In this case, we will call the function named
+      // "nameFound".
+      mdns.setNameResolvedCallback(nameFound); */
+
+    IPAddress ip = WiFi.localIP();
+    Serial.print("AP IP Address: ");
+    Serial.println(ip);
+
+    delay(5000);
+    server.begin();
 }
-// TODO: Change the wifi[0] to another system
+
 /**
- * Function that attempts to connect to the Wi-Fi network.
+ * Function to handle the configuration request and extract SSID and Password
  */
-void connect_WiFi() {
-    while (status != WL_CONNECTED) {
-        Serial.print("Attempting to connect to SSID: ");
-        Serial.println(wifi[0]); 
-        status = WiFi.begin(wifi[0], wifi[1]);
-        delay(5000);  
+void handleConfigRequest()
+{
+    Serial.println("Handling Config Request...");
+    String header = "";
+    String ssid_param = "";
+    String pass_param = "";
 
-        if (status == WL_CONNECTED) {
-            break;  // Exit the loop if connected
+    WiFiClient client = server.available();
+
+    while (client.connected())
+    {
+
+        if (client.available())
+        {
+            char c = client.read();
+
+            Serial.print("Client reading... and client is : ");
+            Serial.println(client.available());
+
+            if (c == '\n')
+            {
+                if (header.indexOf("GET /get?ssid=") >= 0)
+                {
+                    Serial.println("SSID and Password received!");
+                    int ssidIndex = header.indexOf("ssid=") + 5;
+                    int passIndex = header.indexOf("pass=") + 5;
+                    ssid_param = header.substring(ssidIndex, header.indexOf('&'));
+                    pass_param = header.substring(passIndex, header.indexOf(' ', passIndex));
+
+                    ssid_param.toCharArray(ssid, 32);
+                    pass_param.toCharArray(pass, 64);
+                    Serial.println("SSID is : ");
+                    Serial.println(ssid);
+                    Serial.println("Password is : ");
+                    Serial.println(pass);
+
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-type:text/html");
+                    client.println("Connection: close");
+                    client.println();
+                    client.println(generateSuccessPage());
+                    client.println();
+                    Serial.println("Sending Success Page...");
+                    delay(1000);
+                    break;
+                }
+                else if (header.length() == 0)
+                {
+                    Serial.println("Sending Config Page...");
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-type:text/html");
+                    client.println("Connection: close");
+                    client.println();
+                    client.println(generateConfigPage(networks));
+                    client.println();
+                    break;
+                }
+                else
+                {
+                    header = "";
+                }
+            }
+            else if (c != '\r')
+            {                // if you got anything else but a carriage return character,
+                header += c; // add it to the end of the currentLine
+            }
+            Serial.print("Header is : ");
+            Serial.println(header);
         }
-
     }
+
+    if (ssid_param.length() > 0 && pass_param.length() > 0)
+    {
+        needsWiFiConfig = false;
+        connectToWiFi();
+    }
+
+    client.stop();
 }
 
+/**
+ * Function to connect to the Wi-Fi network using the credentials provided by the user.
+ */
+void connectToWiFi()
+{
+    Serial.print("Connecting to WiFi SSID: ");
+    Serial.println(ssid);
+    WiFi.end();
+    delay(1000);
+    WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+
+        status = WiFi.begin(ssid, pass);
+        Serial.print("Wifi status : ");
+        Serial.println(WiFi.status());
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println();
+    Serial.println("Connected to WiFi!");
+
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+    server.begin();
+    delay(1000);
+}
 
 /**
  * Function that handles the HTTP response to display a web page.
  * It receives client requests, sends the HTML page, and manages the disconnection.
  */
-void printWEB(float t, float h, int percentage1, int percentage2) {
-    if (client) {  
-        Serial.println("new client");
-        String currentLine = "";  
-        while (client.connected()) {  
-            if (client.available()) {  
-                char c = client.read();  // Read a character from the request
-                Serial.write(c); 
-                header += c;  // Append the character to the header
+void printWEB()
+{
+    client = server.available();
+    if (client)
+    {
 
-                if (c == '\n') { 
-                    if (currentLine.length() == 0) {
-                        // If the line is blank, it's the end of the HTTP request, so send the response
+        /*
+                if (!client.connected()) {
+                    Serial.println("Client disconnected too soon");
+                    return;
+                } */
+
+        Serial.println("new client yeay");
+        Serial.print("object client : ");
+        Serial.println(client);
+        Serial.print("client connected : ");
+        Serial.println(client.connected());
+        Serial.print("client available : ");
+        Serial.println(client.available());
+        String currentLine = "";
+        while (client.connected())
+        {
+            Serial.println("client connected");
+
+            if (client.available())
+            {
+                Serial.println("client available");
+                Serial.println("------------------------");
+                char c = client.read();
+                Serial.write(c);
+
+                if (c == '\n')
+                {
+                    if (currentLine.length() == 0)
+                    {
                         client.println("HTTP/1.1 200 OK");
                         client.println("Content-type:text/html");
                         client.println("Connection: close");
                         client.println();
 
-                        // Send the HTML page to the client
                         client.println("<!DOCTYPE html><html>");
                         client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
                         client.println("<link rel=\"icon\" href=\"data:,\">");
-                        String webpage = generateWebpage(t, h, percentage1, percentage2);
-                        client.println(webpage);  // Send the generated webpage
-                      
+                        client.println("<h1>Sensor Data Page</h1>");
+                        client.println("</html>");
                         client.println();
 
-                        break;  
-                    } else {  
-                        currentLine = "";  // Reset the current line
+                        break;
                     }
-                } else if (c != '\r') {   
-                    currentLine += c;  // Append the character to the current line if it's not a carriage return
+                    else
+                    { // if you got a newline, then clear currentLine:
+                        currentLine = "";
+                    }
                 }
+                else if (c != '\r')
+                {                     // if you got anything else but a carriage return character,
+                    currentLine += c; // add it to the end of the currentLine
+                }
+                Serial.print("current line is : ");
+                Serial.println(currentLine);
             }
         }
-        header = "";  // Reset the header after processing
-        client.stop();  // Close the connection with the client
-        Serial.println("client disconnected");
     }
+    client.stop();
+    Serial.println("client disconnected nooo");
 }
